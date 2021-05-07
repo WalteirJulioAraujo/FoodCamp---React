8e18d9e30ed14633ac930feae2ad3d8e907fc07b
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-05-07 06:49:40 -0300

    Mensagem de confirmar pedido, funcionando

--- src/Footer/BotaoConfirmar.js
+++ src/Footer/BotaoConfirmar.js
@@ -9,20 +9,20 @@ export default function BotaoConfirmar(props) {
 
   let mensagem = "Olá, gostaria de fazer o pedido:\n"
   let total = 0 ;
-  pratosSelecionado.forEach((e)=>mensagem+=`Prato : ${e.nome} (${e.qtd}x)\n`)
-  bebidasSelecionado.forEach((e)=>mensagem+=`Bebidas : ${e.nome} (${e.qtd}x)\n`)
-  sobremesasSelecionado.forEach((e)=>mensagem+=`Sobremesas : ${e.nome} (${e.qtd}x)\n`)
+  pratosSelecionado.forEach((e)=>mensagem+=`*-Prato* : ${e.nome} (${e.qtd}x)\n`)
+  bebidasSelecionado.forEach((e)=>mensagem+=`*-Bebidas* : ${e.nome} (${e.qtd}x)\n`)
+  sobremesasSelecionado.forEach((e)=>mensagem+=`*-Sobremesas* : ${e.nome} (${e.qtd}x)\n`)
   pratosSelecionado.forEach((e)=>total+=Number((e.preco).replace("R$ ","").replace(",",".")))
   bebidasSelecionado.forEach((e)=>total+=Number((e.preco).replace("R$ ","").replace(",",".")))
   sobremesasSelecionado.forEach((e)=>total+=Number((e.preco).replace("R$ ","").replace(",",".")))
-  mensagem += `\nTotal: ${total.toFixed(2)}`
+  mensagem += `\n*-Total:* R$ ${total.toFixed(2)}`
   mensagem = encodeURIComponent(mensagem)
-  const mensagemFinal=`https://wa.me/21979507925?text=${mensagem}`
+  const mensagemFinal=`https://wa.me/5521979507925?text=${mensagem}`
 
   console.log(mensagem);
   if (controleBotao) {
     return (
-      <a href={mensagemFinal} class="fazer-pedido ativo">
+      <a href={mensagemFinal} class="fazer-pedido ativo" target="_blank">
         Fazer Pedido!
       </a>
     );
